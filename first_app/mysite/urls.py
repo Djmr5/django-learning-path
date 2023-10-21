8cfa10e798530ed8e79eb124ad02144b5b099e48
Diego Jacobo Martínez <diegojmr5@gmail.com>
2023-10-21 15:27:58 -0600

    Project per Folder

---
+++ first_app/mysite/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('calc/', include('calculadora.urls')),
+    path('polls/', include('polls.urls')),
+    path('gviews/', include('generic_views.urls')),
+    path('website/', include('website.urls')),
+    path('acid/', include('acid.urls')),
+    path('admin/', admin.site.urls),
+]
